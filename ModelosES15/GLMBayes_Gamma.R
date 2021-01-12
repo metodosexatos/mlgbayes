1ca9abda62e95ab11cd2584773907c800514c955
Métodos Exatos <37481514+metodosexatos@users.noreply.github.com>
2021-01-11 22:33:43 -0300

    Update GLMBayes_Gamma.R

--- ModelosES15/GLMBayes_Gamma.R
+++ ModelosES15/GLMBayes_Gamma.R
@@ -3,21 +3,21 @@
 # Method: Gamma
 # Case:   Clotting
 # Date:   2021/Jan/11
-# Author: AndrÈ Luis M.F. dos Santos
+# Author: Andr√© Luis M.F. dos Santos
 # e-mail: andre@metodosexatos.com.br
 # Source: www.metodosexatos.com
 #====================================================
 # Base de dados:
 
-# A.Vari·veis originais
+# A.Vari√°veis originais
 
-u <- c(5,10,15,20,30,40,60,80,100)          # concentraÁ„o de plasma (%)
-lot1 <- c(118,58,42,35,27,25,21,19,18)      # tempo de coagulaÁ„o (s)
-lot2 <- c(69,35,26,21,18,16,13,12,12)       # tempo de coagulaÁ„o (s)
+u <- c(5,10,15,20,30,40,60,80,100)          # concentra√ß√£o de plasma (%)
+lot1 <- c(118,58,42,35,27,25,21,19,18)      # tempo de coagula√ß√£o (s)
+lot2 <- c(69,35,26,21,18,16,13,12,12)       # tempo de coagula√ß√£o (s)
 
-#- B.TransformaÁ„o das vari·veis
+#- B.Transforma√ß√£o das vari√°veis
 
-log_plasma <- rep(log(u), 2)                      # logarÌtmo da vari·vel u
+log_plasma <- rep(log(u), 2)                      # logar√≠tmo da vari√°vel u
 clot_time <- c(lot1, lot2)                        # concatenando lot1 e lot2
 lot_id <- factor(rep(c(1,2), each = length(u)))   # se: lot1=1; lot2=2
 
@@ -25,7 +25,7 @@ lot_id <- factor(rep(c(1,2), each = length(u)))   # se: lot1=1; lot2=2
 
 mydata <- data.frame(log_plasma, clot_time, lot_id)
 #------------------------------------------------------
-# Modelo com interaÁ„o:
+# Modelo com intera√ß√£o:
 
 library(rstanarm)
 model_gamma <- stan_glm(clot_time ~ log_plasma * lot_id, data = mydata,
@@ -33,11 +33,12 @@ model_gamma <- stan_glm(clot_time ~ log_plasma * lot_id, data = mydata,
 print(model_gamma, digits = 3)
 
 #------------------------------------------------------
-# Modelo sem interaÁ„o:
+# Modelo sem intera√ß√£o:
 model_gamma1 <- update(model_gamma, formula = . ~ log_plasma)
 
 # Coeficientes:
-coeff <- rbind(model_gamma1$coefficients[1], exp(model_gamma1$coefficients[2]))
-colnames(coeff) <- c("Coeficientes")
-rownames(coeff) <- c("Intercepto", "Plasma")
-coeff
+coef1 <- data.frame(model_gamma1$coefficients, row.names = c("Intercepto", "Plasma"))
+colnames(coef1) <- "Model1"
+coef2 <- data.frame(model_gamma2$coefficients, row.names = c("Intercepto", "Plasma", "Lote"))
+colnames(coef2) <- "Model2"
+merge(coef1, coef2, by="row.names", all = TRUE)
